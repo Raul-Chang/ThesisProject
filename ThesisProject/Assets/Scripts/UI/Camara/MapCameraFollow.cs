a46f647b694d317452527c6620bf0ae829206c80
FacuCesare <facundo.cesare@outlook.es>
2025-09-03 21:23:32 -0300

    fix map camera

--- ThesisProject/Assets/Scripts/UI/Camara/MapCameraFollow.cs
+++ ThesisProject/Assets/Scripts/UI/Camara/MapCameraFollow.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MapCameraFollow : MonoBehaviour
 {
+    [Header("Target (opcional)")]
     public Transform target;
-    public Vector3 offset = new Vector3(0, 40, 0);
+    public Vector3 offset = new Vector3(-19, 253, 14);
+
+    [Header("Opciones")]
+    public bool followTarget = true;
 
     void LateUpdate()
     {
-        if (target != null)
+        if (followTarget && target != null)
         {
+            // Sigue al jugador
             transform.position = target.position + offset;
-            transform.rotation = Quaternion.Euler(90, 0, 0); // top-down fijo
         }
+
+        // Rotación fija top-down
+        transform.rotation = Quaternion.Euler(90, 0, 0);
     }
 }
